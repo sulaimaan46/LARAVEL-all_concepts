60b3d9a0144a68800d947d1c5be35aaea77deb0e
sulai46 <sulaiman@sparkouttech.com>
2022-03-16 18:42:40 +0530

    yaml file CICD

--- deploy.php
+++ deploy.php
@@ -1,62 +1,61 @@
 <?php
-
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'recipe/rsync.php';
 
-set('application', 'My App');
-set('ssh_multiplexing', true);
+// Project name
+set('application', 'Laravel-Sulai');
 
-set('rsync_src', function () {
-    return __DIR__;
-});
+// Project repository
+set('repository', 'https://github.com/Saranraj9531/ci-cd-example.git');
 
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
 
-add('rsync', [
-    'exclude' => [
-        '.git',
-        '/.env',
-        '/storage/',
-        '/vendor/',
-        '/node_modules/',
-        '.github',
-        'deploy.php',
-    ],
-]);
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
 
-task('deploy:secrets', function () {
-    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
-    upload('.env', get('deploy_path') . '/shared');
-});
-
-host('Laravel')
-  ->hostname('3.82.203.16')
-  ->stage('production')
-  ->user('ubuntu')
-  ->set('deploy_path', '/var/www/html');
+// Writable dirs by web server
+add('writable_dirs', []);
 
 
-after('deploy:failed', 'deploy:unlock');
+// Hosts
+host('IP name: ip-172-31-81-127.ec2.internal') // Name of the server
+    ->hostname('3.82.203.16') // Hostname or IP address
+    ->stage('production') // Deployment stage (production, staging, etc)
+    ->user('ubuntu') // SSH user
+    ->set('deploy_path', '/var/www/html'); // Deploy path
+
+after('deploy:failed', 'deploy:unlock'); // Unlock after failed deploy
 
 desc('Deploy the application');
-
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
-    'rsync',
-    'deploy:secrets',
+    'deploy:secrets', // Deploy secrets
     'deploy:shared',
     'deploy:vendors',
     'deploy:writable',
-    'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
-    'artisan:migrate',
-    'artisan:queue:restart',
+    'artisan:storage:link', // |
+    'artisan:view:cache',   // |
+    'artisan:config:cache', // | Laravel specific steps
+    'artisan:optimize',     // |
+    'artisan:migrate',      // |
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
 ]);
+
+// Set up a deployer task to copy secrets to the server.
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
